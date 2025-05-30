8243446cfce90ac8c05f7eaecd87fa571a12fd14
ishaanam <ishaana.misra@gmail.com>
2025-05-30 15:13:19 -0400

    add initial linux setup and cleanup

---
+++ setup_linux.sh
@@ -0,0 +1,5 @@
+echo "Current directory: "
+pwd
+mkdir regtest_datadir
+wget https://bitcoincore.org/bin/bitcoin-core-29.0/bitcoin-29.0-x86_64-linux-gnu.tar.gz
+tar -xvf bitcoin-29.0-x86_64-linux-gnu.tar.gz
