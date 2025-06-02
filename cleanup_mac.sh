f8263ad62dd233015058034b18708846645870ca
Angela Mashuryan <m.angela.0118@gmail.com>
2025-06-02 15:07:45 -0400

    mac setup and cleanup

---
+++ cleanup_mac.sh
@@ -0,0 +1,2 @@
+rm bitcoin-29.0-arm64-apple-darwin-unsigned.tar.gz
+rm -r regtest_data
---
+++ setup_mac.sh
@@ -0,0 +1,5 @@
+echo "Current directory: "
+pwd
+mkdir regtest_data
+wget https://bitcoincore.org/bin/bitcoin-core-29.0/bitcoin-29.0-arm64-apple-darwin-unsigned.tar.gz 
+tar -xvf bitcoin-29.0-arm64-apple-darwin-unsigned.tar.gz
