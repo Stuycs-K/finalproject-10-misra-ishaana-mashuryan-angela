35e333daa714dedd442358dce9ed3fbe47808bce
ishaanam <ishaana.misra@gmail.com>
2025-06-03 10:01:41 -0400

    fix error with ports

--- node.py
+++ node.py
@@ -15,14 +15,15 @@ class Node():
             os.system(f"mkdir {self.datadir}")
         except Exception:
             print("file exists")
-        os.system(f"./{EXEC_DIR}/bitcoind -regtest -datadir={self.datadir} -port={18444+self.num} -rpcport={19443+self.num} -rpcuser={self.name} -rpcpassword={self.num} -daemon")
+        print(f"Number: {self.num}")
+        os.system(f"./{EXEC_DIR}/bitcoind -regtest -datadir={self.datadir} -port={18444+(self.num*2)} -rpcport={8333+self.num} -rpcuser={self.name} -rpcpassword={self.num} -daemon")
 
         NODE_NUMBER += 1
 
     def __del__(self):
         print("shutting down")
         time.sleep(3)
-        os.system(f"./{EXEC_DIR}/bitcoin-cli -datadir={self.datadir} -rpcport={19443+self.num} -rpcuser={self.name} -rpcpassword={self.num} stop")
+        os.system(f"./{EXEC_DIR}/bitcoin-cli -datadir={self.datadir} -rpcport={8333+self.num} -rpcuser={self.name} -rpcpassword={self.num} stop")
 
 if __name__ == '__main__':
     alice = Node("alice")
