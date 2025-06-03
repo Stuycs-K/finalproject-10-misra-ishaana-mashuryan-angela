e0273052bd4ce61c338f320a9d02a610fa93d244
ishaanam <ishaana.misra@gmail.com>
2025-06-03 09:48:20 -0400

    ensure that simulation works with two nodes

--- node.py
+++ node.py
@@ -15,14 +15,15 @@ class Node():
             os.system(f"mkdir {self.datadir}")
         except Exception:
             print("file exists")
-        os.system(f"./{EXEC_DIR}/bitcoind -regtest -datadir={self.datadir} -port={18444+self.num} -rpcport={18443+self.num} -rpcuser={self.name} -rpcpassword={self.num} -daemon")
+        os.system(f"./{EXEC_DIR}/bitcoind -regtest -datadir={self.datadir} -port={18444+self.num} -rpcport={19443+self.num} -rpcuser={self.name} -rpcpassword={self.num} -daemon")
 
         NODE_NUMBER += 1
 
     def __del__(self):
         print("shutting down")
         time.sleep(3)
-        os.system(f"./{EXEC_DIR}/bitcoin-cli -datadir={self.datadir} -rpcport={18443+self.num} -rpcuser={self.name} -rpcpassword={self.num} stop")
+        os.system(f"./{EXEC_DIR}/bitcoin-cli -datadir={self.datadir} -rpcport={19443+self.num} -rpcuser={self.name} -rpcpassword={self.num} stop")
 
 if __name__ == '__main__':
     alice = Node("alice")
+    bob = Node("bob")
