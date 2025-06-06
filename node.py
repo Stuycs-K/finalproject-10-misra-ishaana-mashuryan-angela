abec1e37c4eb6b079b9d55c40574c1b92ba4e4fe
ishaanam <ishaana.misra@gmail.com>
2025-06-06 09:51:44 -0400

    add many node functions

--- node.py
+++ node.py
@@ -10,22 +10,45 @@ class Node():
         global NODE_NUMBER
         self.name = name
         self.num = NODE_NUMBER
+        self.port = 18444+(self.num*2)
+        self.rpcport = 8333+self.num
         self.datadir = BASE_DATADIR + f"{self.num}"
         try:
             os.system(f"mkdir {self.datadir}")
         except Exception:
             print("file exists")
         print(f"Number: {self.num}")
-        os.system(f"./{EXEC_DIR}/bitcoind -regtest -datadir={self.datadir} -port={18444+(self.num*2)} -rpcport={8333+self.num} -rpcuser={self.name} -rpcpassword={self.num} -daemon")
+        os.system(f"./{EXEC_DIR}/bitcoind -regtest -datadir={self.datadir} -port={self.port} -rpcport={self.rpcport} -rpcuser={self.name} -rpcpassword={self.num} -daemon")
 
         NODE_NUMBER += 1
+
+    def connect(self, port):
+        os.system(f"./{EXEC_DIR}/bitcoin-cli -datadir={self.datadir} -rpcport={self.rpcport} -rpcuser={self.name} -rpcpassword={self.num} addnode \"127.0.0.1:{port}\" \"onetry\"")
+
+    def get_nodes(self):
+        os.system(f"./{EXEC_DIR}/bitcoin-cli -datadir={self.datadir} -rpcport={self.rpcport} -rpcuser={self.name} -rpcpassword={self.num} getpeerinfo")
+
+    def create_wallet(self):
+        os.system(f"./{EXEC_DIR}/bitcoin-cli -datadir={self.datadir} -rpcport={self.rpcport} -rpcuser={self.name} -rpcpassword={self.num} createwallet \"{self.name}_wallet\"")
+        os.system(f"./{EXEC_DIR}/bitcoin-cli -datadir={self.datadir} -rpcport={self.rpcport} -rpcuser={self.name} -rpcpassword={self.num} loadwallet \"{self.name}_wallet\"")
+
+    def get_address(self):
+        os.system(f"./{EXEC_DIR}/bitcoin-cli -datadir={self.datadir} -rpcport={self.rpcport} -rpcuser={self.name} -rpcpassword={self.num} getnewaddress")
+
+    def mine_block(self, address):
+        exec_string = f"./{EXEC_DIR}/bitcoin-cli -datadir={self.datadir} -rpcport={self.rpcport} -rpcuser={self.name} -rpcpassword={self.num} generatetoaddress 1 \"{address}\""
+        print(exec_string)
+        # os.system()
+
+    def wallet_balance(self):
+        os.system(f"./{EXEC_DIR}/bitcoin-cli -datadir={self.datadir} -rpcport={self.rpcport} -rpcuser={self.name} -rpcpassword={self.num} getbalance")
 
     # it is better to use a regular function instead of a destructor here because 
     # a destructor would get too complicated while using Jupyter
     def cleanup(self):
         print("shutting down")
         time.sleep(3)
-        os.system(f"./{EXEC_DIR}/bitcoin-cli -datadir={self.datadir} -rpcport={8333+self.num} -rpcuser={self.name} -rpcpassword={self.num} stop")
+        os.system(f"./{EXEC_DIR}/bitcoin-cli -datadir={self.datadir} -rpcport={self.rpcport} -rpcuser={self.name} -rpcpassword={self.num} stop")
 
 if __name__ == '__main__':
     alice = Node("alice")
